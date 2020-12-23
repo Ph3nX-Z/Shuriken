85ac87c72264bafaa8d85f7271caaab52e446083
Ph3nX-Z <66122220+Ph3nX-Z@users.noreply.github.com>
2020-12-23 16:41:31 +0100

    Add files via upload

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ apt install python3-pip
 pip3 install requests
 pip install pyinstaller
 pyinstaller --onefile ./Shuriken.py
+cp ./dist/Shuriken /bin/Shuriken
+echo "[+] Installation Complete !"
 
 
 
