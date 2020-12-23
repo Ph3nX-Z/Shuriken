16041f9d55b896d854d06a6d985485ae19b2f49d
Ph3nX-Z <66122220+Ph3nX-Z@users.noreply.github.com>
2020-12-23 21:16:39 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ apt install python3
 apt install python3-pip
 apt install python-pip
 pip3 install requests
-pip install pyinstaller
+pip3 install pyinstaller
 pyinstaller --onefile ./Shuriken.py
 cp ./dist/Shuriken /bin/shuriken
 echo "[+] Installation Complete !"
