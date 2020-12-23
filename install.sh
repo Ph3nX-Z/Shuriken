9b441c9e7e1a7bbe5ea206b98715c7490f045562
Ph3nX-Z <66122220+Ph3nX-Z@users.noreply.github.com>
2020-12-23 16:30:16 +0100

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,20 +1,8 @@
 
-FILE="/etc/shadow"
-
-if ! [[ $(stat -c "%A" $FILE) =~ "r" ]]; then
-  apt install python3
-  apt install python3-pip
-  pip3 install requests
-  cp ./Shuriken.py /bin/shuriken
-else 
-  echo '[-] Run The Script As Root'
-
-fi
-
-exit 0
-
-
-
+apt install python3
+apt install python3-pip
+pip3 install requests
+cp ./Shuriken.py /bin/shuriken
 
 
 
