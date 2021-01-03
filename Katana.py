d9c0e85cafba097007c7ceae926ad4276d1c71ff
Ph3nX-Z <66122220+Ph3nX-Z@users.noreply.github.com>
2021-01-03 13:12:36 +0100

    Add files via upload

--- Katana.py
+++ Katana.py
@@ -10,6 +10,7 @@ parse = argparse.ArgumentParser()
 parse.add_argument("-w","--wordlist", help="To specify the wordlist", required=True)
 parse.add_argument("-u", "--url", help="To specify the URL", required=True)
 parse.add_argument("-t", "--threads", help="Number of workers (<64)", required=True, type=int)
+parse.add_argument("-r", "--recursive", help="Recursive or not, max recursions", required=False, type=int)
 args = parse.parse_args()
 
 def chunks(myList, parts):
@@ -30,13 +31,39 @@ def fuzzer(part,threads):
         elif response.status_code == 401:
             print('\033[93m' + f"[KATANA] Response : {i} ({response.status_code})[Size : {len(response.content)}] --> Authentication Panel"+'\033[0m')
 
+def fuzzer_recursive(part,threads):
+    with open(args.wordlist,"r") as file:
+        global wordlist
+        wordlist = file.read().split("\n")
+    wordlists = chunks(wordlist,threads)
+    session = requests.Session()
+    for i in wordlists[int(part)]:
+        response = session.get(args.url.replace('NINJA',i),headers={'Cache-Control': 'no-cache',"Pragma": "no-cache"})
+        if response.status_code == 200:
+            print('\033[92m' + f"[KATANA] Response : {i} ({response.status_code})[Size : {len(response.content)}]"+'\033[0m')
+            if '/' in i:
+                directories.append(str(i))
+        elif response.status_code == 403:
+            print('\033[93m' + f"[KATANA] Response : {i} ({response.status_code})[Size : {len(response.content)}]"+'\033[0m')
+            if '/' in i:
+                directories.append(str(i))
+        elif response.status_code == 401:
+            if '/' in i:
+                directories.append(str(i))
+            print('\033[93m' + f"[KATANA] Response : {i} ({response.status_code})[Size : {len(response.content)}] --> Authentication Panel"+'\033[0m')
+
 class multi_fuzz():
     def __init__(self,threads):
         self.threads = threads
     def start(self):
         jobs = []
+        global directories
+        directories = []
         for i in range(self.threads):
-            process = multiprocessing.Process(target=fuzzer,args=(str(i),threads))
+            if args.recursive == None:
+                process = multiprocessing.Process(target=fuzzer,args=(str(i),threads))
+            else:
+                process = multiprocessing.Process(target=fuzzer_recursive,args=(str(i),threads))
             jobs.append(process)
             process = None
     
@@ -45,6 +72,10 @@ class multi_fuzz():
 
         for j in jobs:
             j.join()
+        if args.recursive != None:
+            if len(directories)<args.recursive:
+                for directory in directories:
+                    os.system(f"katana -w {args.wordlist} -u {args.url.replace('NINJA',directory+'NINJA')} -t {args.threads}")
 
 if __name__ == "__main__":
     threads = args.threads
